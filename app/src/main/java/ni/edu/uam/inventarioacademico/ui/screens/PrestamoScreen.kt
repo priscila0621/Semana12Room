5280182f6daa7dc8e90edb0dccf2019a9277fada
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-03 22:10:55 -0600

    feat(ui): crear pantalla de préstamos

---
+++ app/src/main/java/ni/edu/uam/inventarioacademico/ui/screens/PrestamoScreen.kt
@@ -0,0 +1,12 @@
+package ni.edu.uam.inventarioacademico.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PrestamoScreen() {
+
+    Text(
+        text = "Gestión de préstamos"
+    )
+}
