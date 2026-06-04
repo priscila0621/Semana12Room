772881012dc0aa06705ee3394a98429ab810a6cd
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-03 21:59:51 -0600

    feat: implementar ViewModel de equipos con StateFlow

---
+++ app/src/main/java/ni/edu/uam/inventarioacademico/viewmodel/EquipoViewModel.kt
@@ -0,0 +1,41 @@
+package ni.edu.uam.inventarioacademico.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import ni.edu.uam.inventarioacademico.data.local.entity.Equipo
+import ni.edu.uam.inventarioacademico.data.repository.EquipoRepository
+
+class EquipoViewModel(
+    private val repository: EquipoRepository
+) : ViewModel() {
+
+    val equipos = repository.equipos
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            emptyList()
+        )
+
+    fun agregarEquipo(
+        nombre: String,
+        categoria: String,
+        marca: String,
+        serie: String
+    ) {
+
+        viewModelScope.launch {
+
+            repository.insertar(
+                Equipo(
+                    nombre = nombre,
+                    categoria = categoria,
+                    marca = marca,
+                    numeroSerie = serie
+                )
+            )
+        }
+    }
+}
