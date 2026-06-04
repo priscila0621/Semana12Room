463e54b50059fa6a960332dcbd5c3830c9e0a9f3
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-03 22:35:23 -0600

    feat(ui): mejorar visualización del listado de equipos

--- app/src/main/java/ni/edu/uam/inventarioacademico/ui/screens/ListaEquiposScreen.kt
+++ app/src/main/java/ni/edu/uam/inventarioacademico/ui/screens/ListaEquiposScreen.kt
@@ -1,9 +1,14 @@
 package ni.edu.uam.inventarioacademico.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ni.edu.uam.inventarioacademico.data.local.entity.Equipo
 
 @Composable
@@ -15,9 +20,20 @@ fun ListaEquiposScreen(
 
         items(equipos) { equipo ->
 
-            Text(
-                text = "${equipo.nombre} - ${equipo.marca}"
-            )
+            Card(
+                modifier = Modifier.padding(8.dp)
+            ) {
+
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+
+                    Text("Nombre: ${equipo.nombre}")
+                    Text("Categoría: ${equipo.categoria}")
+                    Text("Marca: ${equipo.marca}")
+                    Text("Serie: ${equipo.numeroSerie}")
+                }
+            }
         }
     }
 }
