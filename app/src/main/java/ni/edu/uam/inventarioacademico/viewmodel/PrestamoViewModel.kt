29888f030867ee0198cdda71b04dbea93feb004a
priscila0621 <pjulieths@uamv.edu.ni>
2026-06-03 22:08:29 -0600

    feat(viewmodel): implementar gestión de préstamos

---
+++ app/src/main/java/ni/edu/uam/inventarioacademico/viewmodel/PrestamoViewModel.kt
@@ -0,0 +1,39 @@
+package ni.edu.uam.inventarioacademico.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import ni.edu.uam.inventarioacademico.data.local.entity.Prestamo
+import ni.edu.uam.inventarioacademico.data.repository.PrestamoRepository
+
+class PrestamoViewModel(
+    private val repository: PrestamoRepository
+) : ViewModel() {
+
+    val prestamos = repository.prestamos
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            emptyList()
+        )
+
+    fun registrarPrestamo(
+        equipoId: Int,
+        solicitante: String,
+        fechaPrestamo: String
+    ) {
+
+        viewModelScope.launch {
+
+            repository.insertar(
+                Prestamo(
+                    equipoId = equipoId,
+                    solicitante = solicitante,
+                    fechaPrestamo = fechaPrestamo
+                )
+            )
+        }
+    }
+}
